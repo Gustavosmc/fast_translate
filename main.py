ec6276c250f148e79fea2f2a4a213733d85e6312
Gustavosmc <luizgustavosmc@gmail.com>
2018-10-29 20:16:10 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+try:
+    from google_speech import Speech
+except RuntimeError as re:
+    print(re)
+import clipboard
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
+from googletrans import Translator
+
+control = False
+lang = "pt-BR"
+key_controller = Controller()
+
+
+def speech(text, lang="pt"):
+    translate = Translator()
+    t_text = translate.translate(text, dest='pt').text
+    speech = Speech(t_text, lang)
+    sox_effects = ("speed", "1")
+    speech.play(sox_effects)
+
+
+def do_copy():
+    try:
+        key_controller.press(Key.ctrl_l)
+        key_controller.press('c')
+        key_controller.release('c')
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+def on_press(key):
+    global control
+    global lang
+    if key == Key.ctrl_l:
+        control = True
+    if control and key == Key.space:
+        if do_copy():
+            text = clipboard.paste()
+            speech(text)
+
+
+def on_release(key):
+    global control
+    if key == Key.ctrl_l:
+        control = False
+
+
+with keyboard.Listener(on_press=on_press,
+                       on_release=on_release) as listener:
+    listener.join()
